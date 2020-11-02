de226b3063375ea31f16d41fc7ea4e629db3e897
Bertil Tandayamo <bertilt4@gmail.com>
2020-11-02 14:15:41 -0500

    Refactor to only one method call button

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import PrimaryButton from '../styled-components/primary_button';
+import Button from '../styled-components/button';
 import styles from './signup.module.scss';
+import buttonType from '../styled-components/buttonType';
 
 const SignUp = () => {
+  const { primaryType, linkType } = buttonType;
   const { background, container } = styles;
   return (
     <div className={background}>
       <div className={container}>
         <h1 className="title">Spotify Stats</h1>
         <p>You are what you measure</p>
-        <PrimaryButton title="sign up" />
+        <Button type={primaryType} title="sign up" />
+        <Button type={linkType} title="log in" />
       </div>
     </div>
   );
---
+++ src/styled-components/buttonType.js
@@ -0,0 +1,5 @@
+export default {
+  primaryType: 'PRIMARY',
+  secondaryType: 'SECONDARY',
+  linkType: 'LINK',
+};
