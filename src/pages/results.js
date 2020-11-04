28b0f66b3b5534d9ef5f5f67bce5d5cf5672dd9e
Bertil Tandayamo <bertilt4@gmail.com>
2020-11-04 16:25:31 -0500

    Add body to results page - Add cards items

--- src/pages/results.js
+++ src/pages/results.js
@@ -3,29 +3,56 @@ import PhoneContainer from '../components/phone-container';
 import artistItems from '../assets/artistItems';
 import './results.scss';
 
-const Results = () => (
-  <PhoneContainer tabActive="2">
-    <div className="results-page">
-      <div className="results-content">
-        <div className="header-results-page">
-          <h3>Popular artists</h3>
-          <div className="popular-artists">
+const Results = () => {
+  const styles = {
+    background: 'red',
+  };
+
+  return (
+    <PhoneContainer tabActive="2">
+      <div className="results-page">
+        <div className="results-content">
+          <div className="header-results-page">
+            <h3>Popular artists</h3>
+            <div className="popular-artists">
+              {artistItems.map(elem => (
+                <div key={elem.id} className="popular-artists-element">
+                  <img src={elem.photoUrlIcon} width="68" alt={elem.artistName} />
+                  <p>{elem.artistName}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="body-results-page">
             {artistItems.map(elem => (
-              <div key={elem.id} className="popular-artists-element">
-                <img src={elem.photoUrlIcon} width="68" alt={elem.artistName} />
-                <p>{elem.artistName}</p>
+              <div key={elem.id} style={styles} className="card-artist-container">
+                <div className="card-artist-content">
+                  <img src={elem.photoUrlIcon} alt={elem.artistName} width="120" />
+                  <div className="info-artista">
+                    <h3>{elem.artistName}</h3>
+                    <div className="stats">
+                      <div className="stream">
+                        <span>STREAMS</span>
+                        <p>{elem.stats.streams}</p>
+                      </div>
+                      <div className="hours">
+                        <span>HOURS</span>
+                        <p>{elem.stats.hours}</p>
+                      </div>
+                      <div className="days">
+                        <span>DAYS</span>
+                        <p>{elem.stats.days}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
         </div>
-        <div className="body-results-page">
-          <div className="card-artist">
-            <p>hola</p>
-          </div>
-        </div>
       </div>
-    </div>
-  </PhoneContainer>
-);
+    </PhoneContainer>
+  );
+};
 
 export default Results;
