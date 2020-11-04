74cd0f186cd985d6c0c8c43512d9ca54db09fa2f
Bertil Tandayamo <bertilt4@gmail.com>
2020-11-04 12:45:50 -0500

    Add naigation to tab nav element - Update index css to index scss

--- src/components/tab-navigation.js
+++ src/components/tab-navigation.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import IconsSvg from '../assets/icons/icons.svg';
 import './tab-navigation.scss';
 
 const TabNavigation = ({ active }) => (
   <div className="container-tab">
     <div className="content-tab">
-
       <div className={`icon library ${active === '1' && 'active'}`}>
-        <svg className="library-icon">
-          <use href={`${IconsSvg}#library`} />
-        </svg>
-        <span>Library</span>
+        <Link to="/artists">
+          <svg className="library-icon">
+            <use href={`${IconsSvg}#library`} />
+          </svg>
+          <span>Library</span>
+        </Link>
       </div>
 
       <div className={`icon results ${active === '2' && 'active'}`}>
-        <svg className="results-icon">
-          <use href={`${IconsSvg}#results`} />
-        </svg>
-        <span>Results</span>
+        <Link to="/results">
+          <svg className="results-icon">
+            <use href={`${IconsSvg}#results`} />
+          </svg>
+          <span>Results</span>
+        </Link>
       </div>
 
       <div className={`icon profile ${active === '3' && 'active'}`}>
