93a17be7d3ca9705e04bb2e703ba97a3f57b217f
Bertil Tandayamo <bertilt4@gmail.com>
2020-11-03 22:06:11 -0500

    Add back arrow button to artist info component

--- src/pages/artist-Info.js
+++ src/pages/artist-Info.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import PhoneContainer from '../components/phone-container';
+import IconsSvg from '../assets/icons/icons.svg';
 import './artist-info.scss';
 
 const ArtistInfo = ({ location }) => {
@@ -14,13 +16,19 @@ const ArtistInfo = ({ location }) => {
     background: `url(${photoUrlCover})`,
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
-    height: '300px',
+    height: '200px',
+    opacity: '0.5',
   };
 
   return (
     <PhoneContainer tabActive="1">
       <div className="header-artist-info">
         <div className="hero-inner" style={backgroundImage} />
+        <Link to="/artists">
+          <svg className="back-arrow-icon">
+            <use href={`${IconsSvg}#back-arrow`} />
+          </svg>
+        </Link>
         <p>{artistName}</p>
       </div>
     </PhoneContainer>
