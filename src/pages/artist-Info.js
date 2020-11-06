9c625e849dc6f02344780b8addbf600baa411dc5
Bertil Tandayamo <bertilt4@gmail.com>
2020-11-06 13:24:43 -0500

    Add loading component - To wait while data is loading

--- src/pages/artist-Info.js
+++ src/pages/artist-Info.js
@@ -3,43 +3,56 @@ import PropTypes from 'prop-types';
 import { getArtist, getArtistTopTracks } from '../logic-operations/Api';
 import ArrowBack from '../components/arrow-back';
 import PhoneContainer from '../components/phone-container';
+import TagMessage from '../components/tag-message';
 import './artist-info.scss';
 
 const ArtistInfo = ({ match }) => {
   const { params } = match;
+  const [loading, setLoading] = useState(true);
   const [artist, setArtist] = useState([]);
   const [tracks, setTracks] = useState([]);
 
   useEffect(() => {
     Promise.all([getArtist(params.id), getArtistTopTracks(params.id)]).then(data => {
+      if (data !== null) setLoading(false);
       setArtist(data[0]);
       setTracks(data[1].tracks);
     });
   }, []);
 
   let trackNumber = 0;
+  const artistTracks = artist.length !== 0 && tracks.length !== 0;
   return (
     <PhoneContainer tabActive="1">
-      <div className="header-artist-info">
-        <div className="hero-inner" style={{ backgroundImage: `url(${artist.images?.[0].url})` }} />
-        <ArrowBack path="/artists" />
-        <p>{artist.name}</p>
-      </div>
-      <div className="body-artist-info">
-        <h3>Popular</h3>
-        <div className="popular-items">
-          {tracks?.map(elem => {
-            trackNumber += 1;
-            return (
-              <div key={elem.id} className="track-item">
-                <span>{trackNumber}</span>
-                <img src={elem.album.images?.[elem.album.images.length - 1].url} alt={elem.trackName} width="45" />
-                <p>{elem.name}</p>
+      {artistTracks && !loading
+        && (
+          <>
+            <div className="header-artist-info">
+              <div className="hero-inner" style={{ backgroundImage: `url(${artist.images?.[0].url})` }} />
+              <p>{artist.name}</p>
+              <ArrowBack path="/artists" />
+            </div>
+            <div className="body-artist-info">
+              <h3>Popular</h3>
+              <div className="popular-items">
+                {tracks?.map(elem => {
+                  trackNumber += 1;
+                  return (
+                    <div key={elem.id} className="track-item">
+                      <span>{trackNumber}</span>
+                      <img src={elem.album.images?.[elem.album.images.length - 1].url} alt={elem.trackName} width="45" />
+                      <p>{elem.name}</p>
+                    </div>
+                  );
+                })}
               </div>
-            );
-          })}
-        </div>
-      </div>
+            </div>
+          </>
+        )}
+      {loading
+        && (
+          <TagMessage title="Loading..." />
+        )}
     </PhoneContainer>
   );
 };
