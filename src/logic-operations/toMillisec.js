2a15504d15bb72693e6a56b01464129f977a2a48
Bertil Tandayamo <bertilt4@gmail.com>
2020-11-10 18:04:34 -0500

    Display result stats - Add text align right - Add loading message

--- src/logic-operations/toMillisec.js
+++ src/logic-operations/toMillisec.js
@@ -3,9 +3,9 @@ export default duration => {
   let minutes = Math.floor((duration / (1000 * 60)) % 60);
   let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
-  hours = (hours < 10) ? `0 ${hours}` : hours;
-  minutes = (minutes < 10) ? `0 ${minutes}` : minutes;
-  seconds = (seconds < 10) ? `0 ${seconds}` : seconds;
+  hours = (hours < 10) ? `0${hours}` : hours;
+  minutes = (minutes < 10) ? `0${minutes}` : minutes;
+  seconds = (seconds < 10) ? `0${seconds}` : seconds;
 
   return `${hours}:${minutes}:${seconds}`;
 };
--- src/pages/result-tracks.js
+++ src/pages/result-tracks.js
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import ArrowBack from '../components/arrow-back';
 import IconsSvg from '../assets/icons/icons.svg';
 import PhoneContainer from '../components/phone-container';
 import { getFormatDate, getMonthString } from '../logic-operations/date-format';
 import './result-tracks.scss';
+import { getArtistStats } from '../logic-operations/Api';
+import toMillisec from '../logic-operations/toMillisec';
+import TagMessage from '../components/tag-message';
 
-const ResultTrack = ({ location }) => {
-  const { state } = location;
+const ResultTrack = () => {
+  const params = useParams();
   const [date, setDate] = useState(new Date().getMonth());
+  const [stats, setStats] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const setDatesFunction = click => {
     if (click === 1 && date !== 0) {
@@ -18,66 +23,77 @@ const ResultTrack = ({ location }) => {
     }
   };
 
-  const {
-    artistName,
-    tracking,
-  } = state;
+  useEffect(() => {
+    getArtistStats(params.id).then(response => {
+      if (response.status === 200) {
+        setLoading(false);
+        console.log(response.data);
+        setStats(response.data);
+      }
+    });
+  }, []);
+
   return (
     <PhoneContainer tabActive="2">
-      <div className="result-track-container">
-        <div className="result-track-content">
-          <div className="header result-track-content-header">
-            <ArrowBack path="/results" />
-          </div>
-          <div className="result-track-content-body">
-            <h2>{artistName}</h2>
-            <div className="pick-date">
-              <div
-                className="left-arrow"
-                onClick={() => setDatesFunction(1)}
-                onKeyUp={() => { }}
-                role="button"
-                tabIndex="0"
-              >
-                <svg className="icon left-arrow-icon">
-                  <use href={`${IconsSvg}#back-arrow`} />
-                </svg>
+      {stats.length !== 0 && !loading
+        ? (
+          <div className="result-track-container">
+            <div className="result-track-content">
+              <div className="header result-track-content-header">
+                <ArrowBack path="/results" />
               </div>
-              <div className="text-date">{getMonthString(date)?.toUpperCase()}</div>
-              <div
-                className="right-arrow"
-                onClick={() => setDatesFunction(2)}
-                onKeyUp={() => { }}
-                role="button"
-                tabIndex="0"
-              >
-                <svg className="icon right-arrow-icon">
-                  <use href={`${IconsSvg}#back-arrow`} />
-                </svg>
-              </div>
-            </div>
-            <div className="result-tracking-info">
-              {tracking.map(elem => (
-                <div key={elem.id} className="result-tracking-item">
-                  <div className="date">
-                    <h4>{getFormatDate(elem.date)}</h4>
-                    <p>{elem.trackName}</p>
+              <div className="result-track-content-body">
+                <h2>{stats.data.attributes.name}</h2>
+                <div className="pick-date">
+                  <div
+                    className="left-arrow"
+                    onClick={() => setDatesFunction(1)}
+                    onKeyUp={() => { }}
+                    role="button"
+                    tabIndex="0"
+                  >
+                    <svg className="icon left-arrow-icon">
+                      <use href={`${IconsSvg}#back-arrow`} />
+                    </svg>
                   </div>
-                  <div className="hours">
-                    <p>{`${elem.hours}h`}</p>
+                  <div className="text-date">{getMonthString(date)?.toUpperCase()}</div>
+                  <div
+                    className="right-arrow"
+                    onClick={() => setDatesFunction(2)}
+                    onKeyUp={() => { }}
+                    role="button"
+                    tabIndex="0"
+                  >
+                    <svg className="icon right-arrow-icon">
+                      <use href={`${IconsSvg}#back-arrow`} />
+                    </svg>
                   </div>
                 </div>
-              ))}
+                <div className="result-tracking-info">
+                  {stats.included.map(elem => {
+                    const { attributes } = elem;
+                    return (
+                      <div key={elem.id} className="result-tracking-item">
+                        <div className="date">
+                          <h4>{getFormatDate(attributes.created_at)}</h4>
+                          <p>{attributes.track_name}</p>
+                        </div>
+                        <div className="hours">
+                          <p>{`${toMillisec(attributes.hours)}`}</p>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        )
+        : (
+          <TagMessage title="Loading..." />
+        )}
     </PhoneContainer>
   );
-};
-
-ResultTrack.propTypes = {
-  location: PropTypes.objectOf(PropTypes.objectOf).isRequired,
 };
 
 export default ResultTrack;
