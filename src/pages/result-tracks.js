0979908d6f89ca01bca23d24ddc25b287722e210
Bertil Tandayamo <bertilt4@gmail.com>
2020-11-11 01:45:02 -0500

    Filter by date - According to a passed month display the stats

--- src/pages/result-tracks.js
+++ src/pages/result-tracks.js
@@ -12,17 +12,42 @@ import groupBy from '../logic-operations/groupBy';
 import makeArray from '../logic-operations/makeArray';
 import sumTotal from '../logic-operations/sumTotal';
 
+const checkArgMonth = (dateString, argMonth) => {
+  let response = false;
+  const date = new Date(dateString);
+  const month = date.getMonth();
+  if (month === argMonth) response = true;
+  return response;
+};
+
+const getbyDate = (statsGroupedArray, checkDate) => statsGroupedArray.map(
+  elem => ({
+    track: elem.track.filter(elem => {
+      const { attributes: { created_at: createdAt } } = elem;
+      return checkArgMonth(createdAt, checkDate);
+    }),
+  }),
+);
+
 const ResultTrack = () => {
   const params = useParams();
   const [date, setDate] = useState(new Date().getMonth());
   const [stats, setStats] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [statsGroup, setStatsGroup] = useState([]);
 
   const setDatesFunction = click => {
+    let dateLoc;
     if (click === 1 && date !== 0) {
       setDate(date - 1);
+      dateLoc = date - 1;
     } else if (click === 2 && date !== 11) {
       setDate(date + 1);
+      dateLoc = date + 1;
+    }
+    if (date <= new Date().getMonth()) {
+      const included = getbyDate(statsGroup, dateLoc);
+      setStats({ ...stats, included });
     }
   };
 
@@ -32,7 +57,10 @@ const ResultTrack = () => {
         setLoading(false);
         let statsGrouped = groupBy(response.data.included, 'spotify_track_id');
         statsGrouped = makeArray(statsGrouped);
-        setStats({ name: response.data.data.attributes.name, included: statsGrouped });
+        const { data: { data: { attributes: { name } } } } = response;
+        setStatsGroup(statsGrouped);
+        const included = getbyDate(statsGrouped, date);
+        setStats({ name, included });
       }
     });
   }, []);
@@ -74,9 +102,9 @@ const ResultTrack = () => {
                   </div>
                 </div>
                 <div className="result-tracking-info">
-                  {stats.included.map(elem => {
+                  {stats.included.filter(elem => elem.track.length !== 0).map(elem => {
                     const { track } = elem;
-                    const { attributes } = track[0];
+                    const { attributes } = track?.[0];
                     return (
                       <div key={track[0].id} className="result-tracking-item">
                         <div className="date">
